4a864a652eb69dbe8517f3078e2f43751132f5ba
shyam sundar baitha <avik96045@gmail.com>
2026-05-31 22:34:35 +0545

    Update auth.js

--- cee/auth.js
+++ cee/auth.js
@@ -12,7 +12,7 @@ const {
 } = await supabase.auth.getSession();
 
 if (!session) {
-  window.location.href = "/login.html";
+  window.location.href = "/cee/login.html";
 }
 
 // Show user info
@@ -30,7 +30,7 @@ const logoutBtn = document.getElementById("logoutBtn");
 if (logoutBtn) {
   logoutBtn.addEventListener("click", async () => {
     await supabase.auth.signOut();
-    window.location.href = "/login.html";
+    window.location.href = "/cee/login.html";
   });
 }
 </script>
